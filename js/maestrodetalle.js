659f67b80ac9a15fb010940446e293ba683f0db8
InmaG <inmags20@outlook.es>
2021-03-02 15:53:23 +0100

    modify all

--- js/maestrodetalle.js
+++ js/maestrodetalle.js
@@ -1,4 +1,4 @@
-console.log("hola");
+
 //crear una funcion que nos muestre información de una api
 
 // function fetchMonty (character){
@@ -20,41 +20,93 @@ console.log("hola");
 
 let contenido = document.querySelector('#contenido');
 
-function traer(){
-    fetch(`https://pokeapi.co/api/v2/5/`)
-    .then(response =>response.json())
-    .then(data => {
-        console.log(data.results)
+// function traer(){
+//     fetch(`https://pokeapi.co/api/v2/pokemon/5/`)
+//     .then(response =>response.json())
+//     .then(data => {
+//         console.log(data)
+//     })
+// }
+
+// traer()
+// let name = document.getElementById("name");
+// function traerPokemon(name){
+    
+//     fetch(`https://pokeapi.co/api/v2/pokemon/`)
+//     .then(response => response.json())
+//     .then(data =>{
+//         console.log(data)
+//     })
+// }
+// traerPokemon()
+let lista= document.querySelector("#listaPokemon");
+let busqueda =document.querySelector("#searchterm");
+let btn= document.querySelector("#search");
+
+btn.addEventListener("click", ()=> consultarPokemon(busqueda.value.toLowerCase()));
+
+
+//funcion para consultar los diferentes Pokemon.
+function consultarPokemon(id){
+    fetch (`https://pokeapi.co/api/v2/${id}`)
+    .then(function(response){
+         response.json()
+            .then(function(pokemon){
+                pintarpokemon(pokemon.results)
+                // crearPokemon(pokemon.name)
+            })
+        
     })
+
 }
 
-traer()
+function pintarpokemon(pokemons){
+    console.log(pokemons)
+      
+pokemons.map((pokemon)=>{
+    let pokemonDiv = document.createElement('div');
+    pokemonDiv.classList.add("resultados")
+    let pokemonText = document.createTextNode(pokemon.name)
+    pokemonDiv.appendChild(pokemonText);
+    listaPokemon.appendChild(pokemonDiv);
+    pokemonDiv.addEventListener("click", ()=>recogerSeleccion(pokemon.name))
+})
 
-// let lista = document.getElementsById("listaPokemon");
+}
 
-// //funcion para consultar los diferentes Pokemon.
-// function consultarPokemon(id, num){
-//     fetch (`https://pokeapi.co/api/v2/${id}`)
-//     .then(function(response){
-//          response.json()
-//             .then(function(pokemon){
-//                 crearPokemon(pokemon, num)
-//             })
+function recogerSeleccion (pokemonName){
+    console.log(pokemonName)
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    .then(response=>response.json())
+    .then(data => pintarSeleccion(data.abilities))
+
+}
+
+function pintarSeleccion (pokemonSeleccionado, ){
+    pokemonSeleccionado.map(habilidades=>{
+        let pokemonDiv = document.createElement('div');
+        pokemonDiv.classList.add("habilidades");
+               
+        let pokemonHabilidades = document.createTextNode(`habilidad ${habilidades.ability.name}`);
+        console.log(habilidades.ability)
+        pokemonDiv.appendChild(pokemonHabilidades);
+        listaPokemon.appendChild(pokemonDiv);
+               
         
-//     })
+           })
+    // pokemonImagenes.map(image =>{
+        let pokemonImg = document.createElement("img");
+        pokemonImg.setAttribute("src", data.sprites.front_default);
+        pokemonImg.classList.add("image");
 
-// }
+        let pokemonImagenes = document.createTextNode(`image ${image.sprites.front_shine}`);
+        console.log(image.ability)
+        listaPokemon.appendChild(pokemonDiv);
 
+    // })
+   
+    
+ console.log(pokemonSeleccionado);
+}
 
-// funtion consultarPokemones (){
-//     let primerId = Math.round(Math.random()*150)
-//     let segundoId = Math.round(Math.random()*150)
-
-//     consultarPokemon(primerId, 1);
-//     consultarPokemon(segundoId, 2);
-
-// }
-// function crearPokemon(pokemon, num){
-
-// }
-// consultarPokemones();
